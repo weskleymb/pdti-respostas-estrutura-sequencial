470cabf72386e882ed699c1a76492970e47c40e5
Weskley Bezerra <weskleymb@hotmail.com>
2020-08-04 14:44:05 -0300

    primeiro commit

---
+++ resposta02.py
@@ -0,0 +1,8 @@
+# entrada do numero
+numero = input("Informe um número ")
+
+# conversão da string para numero inteiro
+numero = int(numero)
+
+# saida de dados
+print("O número informado foi", numero)
---
+++ resposta03.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Informe o número 1: "))
+
+numero2 = int(input("Informe o número 2: "))
+
+soma = numero1 + numero2
+
+print("A soma dos números é", soma)
